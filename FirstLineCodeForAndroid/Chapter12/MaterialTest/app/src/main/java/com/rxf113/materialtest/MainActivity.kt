3243f3560f0cdeccc9c01dc9830b593a3b293510
我是个文盲 <1131310577@qq.com>
2021-10-02 22:07:43 +0800

    fix mainActivity

--- FirstLineCodeForAndroid/Chapter12/MaterialTest/app/src/main/java/com/rxf113/materialtest/MainActivity.kt
+++ FirstLineCodeForAndroid/Chapter12/MaterialTest/app/src/main/java/com/rxf113/materialtest/MainActivity.kt
@@ -36,6 +36,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
 
 
         initFruits()
